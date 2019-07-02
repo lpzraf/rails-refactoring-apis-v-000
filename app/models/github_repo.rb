a7f743b7f1107ab2a6f5951b6c92b2e381bc2e89
lpzraf <lpz.raf@gmail.com>
2019-07-02 09:59:31 -0400

    Done.

--- app/models/github_repo.rb
+++ app/models/github_repo.rb
@@ -1,8 +1,8 @@
 class GithubRepo
   attr_accessor :name, :url
 
-  def initialize(name, url)
-     @name = name
-     @url = html_url
+  def initialize(hash)
+     hash.name = name
+     hash.url = html_url
   end
 end
